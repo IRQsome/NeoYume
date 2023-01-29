85195d783f49f6e66bb6c7ef5522ddd7326554c6
Ada Gottensträter <nunuu@gmx.net>
2023-01-29 23:03:52 +0100

    Add new fatfs settings

---
+++ build_comptest.sh
@@ -0,0 +1,3 @@
+../spin2cpp/build/flexspin -2 -E -H 376832 -O1,extrasmall,inline-single,loop-reduce,experimental -DFF_FS_TINY=1 -DFF_FS_READONLY=1 --charset=oem neoyume_upper.spin2
+../spin2cpp/build/flexspin -2 -E -H 233472 -O1,extrasmall,inline-single,loop-reduce,experimental --charset=oem neoyume_input.spin2
+../spin2cpp/build/flexspin -2 -l neoyume_lower.spin2
