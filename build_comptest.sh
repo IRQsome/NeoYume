07d01156586637df7f3fe8bed360cceb86f5d4c4
Ada Gottensträter <nunuu@gmx.net>
2024-03-06 20:06:23 +0100

    enable compression for test build scripts

--- build_comptest.sh
+++ build_comptest.sh
@@ -1,3 +1,3 @@
 ../spin2cpp/build/flexspin -2 -E -H 376832 -O1,extrasmall,inline-single,loop-reduce,experimental,aggressive-mem -DFF_FS_TINY=1 -DFF_FS_READONLY=1 --charset=oem neoyume_upper.spin2
 ../spin2cpp/build/flexspin -2 -E -H 233472 -O1,extrasmall,inline-single,loop-reduce,remove-bss,experimental,~fcache --charset=oem neoyume_input.spin2
-../spin2cpp/build/flexspin -2 -l neoyume_lower.spin2
+../spin2cpp/build/flexspin -2 -l --compress neoyume_lower.spin2
