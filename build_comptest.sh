98d3efbac74ad612d19a9875c9f5577e57d56f2d
Ada Gottensträter <nunuu@gmx.net>
2025-02-11 17:45:52 +0100

    Improve build scripts

--- build_comptest.sh
+++ build_comptest.sh
@@ -1,3 +1,5 @@
+set -e # Enable error reporting
+rm neoyume_lower.binary || true
 ../spin2cpp/build/flexspin -2 -E -H 376832 -O1,extrasmall,inline-single,loop-reduce,experimental,aggressive-mem,merge-duplicate-functions -DFF_FS_TINY=1 -DFF_FS_READONLY=1 --charset=oem neoyume_upper.spin2
 ../spin2cpp/build/flexspin -2 -E -H 233472 -O1,extrasmall,inline-single,loop-reduce,remove-bss,experimental,~fcache --charset=oem neoyume_input.spin2
 ../spin2cpp/build/flexspin -2 -l --compress neoyume_lower.spin2
